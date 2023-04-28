cd2f2695a72f51466f76f91296a587d848896ec4
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-04-28 20:12:29 +0700

    Tugas 17 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas17_1.js
@@ -0,0 +1,13 @@
+function ubah(){
+
+    let kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+    let kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+
+    console.log(kalimat1.toLowerCase());
+    console.log(kalimat1.toUpperCase());
+
+    console.log(kalimat2.toLowerCase());
+    console.log(kalimat2.toUpperCase());
+}
+
+ubah();
---
+++ tugas17_2.js
@@ -0,0 +1,21 @@
+function ascii(){
+
+    let kalimat = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    // console.log(kalimat.charCodeAt(0));
+
+    //  for (let i = 0; i < 6; i++) {
+    //     console.log(kalimat.charCodeAt(i));
+        
+    //  }
+
+}
+
+ascii();
+
+const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  const array = [];
+  for (let i = 0; i < 6; i++) {
+    array.push(text.charCodeAt(i));
+  }
+
+  console.log(array);
